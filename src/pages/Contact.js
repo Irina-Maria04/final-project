df940997a885c4f40644f0276936b258675496c4
Irina-Maria04 <irina.bazavan04@gmail.com>
2021-03-02 18:32:29 +0200

    interactive map

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,7 +17,6 @@ import {
   faAddressCard,
   faClock,
 } from "@fortawesome/free-solid-svg-icons";
-import img from "./adresa.png";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 
@@ -63,12 +62,20 @@ const Contact = () => {
           </ContactForm>
         </Container>
         <Map>
-          <a
-            href="https://www.google.com/maps/place/Nautilus+English+Books/@44.459005,26.08133,16z/data=!4m5!3m4!1s0x0:0xf77a1114222ec00e!8m2!3d44.4590052!4d26.0813304?hl=en"
+          {/* <a
+            //href="https://www.google.com/maps/place/Nautilus+English+Books/@44.459005,26.08133,16z/data=!4m5!3m4!1s0x0:0xf77a1114222ec00e!8m2!3d44.4590052!4d26.0813304?hl=en"
             target={"_blank"}
           >
             <img src={img} alt="map" />
-          </a>
+          </a> */}
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5695.479740554517!2d26.08133!3d44.459005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf77a1114222ec00e!2sNautilus%20English%20Books!5e0!3m2!1sen!2sro!4v1614702371514!5m2!1sen!2sro"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowfullscreen=""
+            loading="lazy"
+          ></iframe>
         </Map>
       </ContactWrapper>
       <Footer />
